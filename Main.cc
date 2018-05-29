9186c6a0afff0763b11b23153fd03ffab792883c
claudio <claudio@Dirac>
2018-05-29 01:24:06 -0400

    template polinomio y main con problema dos de la tarea

--- Main.cc
+++ Main.cc
@@ -1,25 +1,39 @@
 #include <iostream>
 #include "Polinomio.h"
+#include "cmath"
+#include <fstream>
 
 using namespace std;
 
 int main(){
-  vector<double> v1 = {1,1,1,1,1};
-  vector<double> v2 = {1,1,1,1,1};
-  Polinomio M(4);
-  Polinomio G(4,v1);
-  
-  M.set_polinomio(v1);
-  G.set_polinomio(v2);
-  //cout <<M<<endl;
-  //cout <<G<<endl;
-  cout <<M<<endl;
-  //cout <<2*G<<endl;
-  
+  vector<double> v1 = {-4,2,-5,1};
+  vector<double> v2 = {8,7,3};
+  Polinomio<double> num(v1);
+  Polinomio<double> den(v2);
+  int N=30; 
+  ofstream archivo("datos.dat"); 
+  double contador = -5.0;
+  for (int i=0; i<N;++i){
+    ofstream archivo("datos.dat", ios::app);
+    double Num= num(contador);
+    double Den= den(contador); 
+    archivo <<contador <<" "<<cos(exp(Num/Den))<<endl;
+    //cout<<contador<<endl;
+    contador += (10./(N-1));
+  }
+  archivo.close();
 
-
+  //nodos de Chebyshev
+  ofstream archivo_Chebyshev("datos_Chebyshev.dat"); 
+  for (int i=1; i<(N+1);++i){
+    ofstream archivo_Chebyshev("datos_Chebyshev.dat", ios::app);
+    double contador = 5*cos(((2*i-1)*M_PI)/(2*N)); 
+    double Num= num(contador);
+    double Den= den(contador);
+    archivo_Chebyshev <<contador <<" "<<cos(exp(Num/Den))<<endl;
+    }
+  archivo_Chebyshev.close();
   
-   
-  return 0;
+ return 0;
 }
 
--- Polinomio.h
+++ Polinomio.h
@@ -1,102 +1,245 @@
+#ifndef POLINOMIO_H
+#define POLINOMIO_H
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
 
+template <class T>
 class Polinomio{
  private:
   int grado;
-  vector<double> pol; 
+  vector<T> pol; 
 
  public:
-  Polinomio(int,vector<double>);
+  Polinomio(vector<T>);
   ~Polinomio();
-  void set_polinomio(vector<double>);
+  void set_polinomio(vector<T>);
   int get_grado();
-  vector<double> get_pol(); 
+  vector<T> get_pol();
+  vector<T> div(Polinomio);
+  T Coef(int) const; 
+  T operator()(T) const;
+  Polinomio<T> derivada();
+  Polinomio<T> integral(T);
+  
+  
 
 };
 
+#endif
+
+
+/*############################################################ FUNCIONES ##################################################################################*/
 //Constructor con default
-Polinomio::Polinomio(int a,vector<double> B = {0}) {
-  grado=a;
-  if (A.size() == (grado+1)){
-    pol = A;
-    }
-  else {
-    for (int i=0;i<grado+1;++i)
-      {
-	pol.push_back(0);
-      }
-  }
+template <class T>
+Polinomio<T>::Polinomio(vector<T> B ){
+  pol = B;
   
 }
-
 //Destructor
-Polinomio::~Polinomio(){
+template <class T>
+Polinomio<T>::~Polinomio(){
 }
 
-int Polinomio::get_grado(){
-  return grado;  
+//funcion entrega el grado del polinomio 
+template <class T>
+int Polinomio<T>::get_grado(){ 
+  return pol.size()-1;
 }
-vector<double> Polinomio::get_pol(){
+//funcion entrega el vector que contiene los coeficiente del polinomio  
+template <class T>
+vector<T> Polinomio<T>::get_pol(){
   return pol; 
 }
-  
-void Polinomio::set_polinomio(vector<double> A){
+
+//funcion puede asignar un polinomio a partir de un vector.  
+template <class T>
+void Polinomio<T>::set_polinomio(vector<T> A){
   pol=A; 
 }
 
-//Para mostrar un polinomio
-ostream & operator << (ostream & os ,Polinomio M){
-  int grado=M.get_grado();
-  string r = "";
-  vector<double> pol=M.get_pol(); 
-  for (int i=0;i<grado+1;++i){
-    
+//funcion que entrega el coeficiente que se le solicita
+template <class T>
+T Polinomio<T>::Coef(int i) const{
+  return pol[i];
+}
+
+//Funcion que deriva el polinomio dado 
+template <class T>
+Polinomio<T> Polinomio<T>::derivada(){
+  vector<T> derivada;
+  vector<T> v_X= pol;
+  for (int i=1; i<(v_X.size()); ++i){
+    derivada.push_back((i)*v_X[i]);
   }
+  Polinomio Derivada(derivada); 
+  return Derivada;
+}
+//funcion que integra el polinomio dado, al que se le agrega una constante.Siempre agregar constante inclusive si es cero.  
+template <class T>
+Polinomio<T> Polinomio<T>::integral(T constante){
+  vector<T> integral;
+  integral.push_back(constante); 
+  vector<T> v_X= pol;
+  for (int i=1; i<(v_X.size()+1); ++i){
+    integral.push_back(v_X[i-1]/i);
+  }
+  Polinomio<T> Integral(integral); 
+  return Integral;
+}
+
+/*########################################## OPERADORES #################################################################################################*/
+
+//operador para evaluar un valor x en el polinomio
+template <class T>
+T Polinomio<T>::operator()(T x) const
+{
+  //algoritmo de honner
+  int grado = pol.size()-1; 
+  T suma = 0;
+  for (int i = grado; i > 0; i--){
+    suma = x * (pol[i] + suma);
+  }
+  suma += pol[0];
+  return suma;
+}
+
+//Operador cout para mostrar un polinomio
+template <class T>
+ostream & operator << (ostream & os ,Polinomio<T> M){
+  int grado=M.get_grado();
+  vector<T> pol=M.get_pol();
+  
+  if (pol[0]!=0){
+    os << pol[0]; 
+  }
+  for (int i=1;i<grado+1;++i){
+    if (pol[i]!=0){
+      if (pol[i]>0){
+	os << "+";
+      }
+      os << pol[i];
+      if(i==0){
+	os << "";
+      }
+      else if(i==1){
+	os << "x";
+      }
+      else {
+	os << "x^"<<i; 
+	}
+    }
+    else {
+    }    
+  }
+  os << endl; 
   return os;
 }
 
-/*//Operador Suma
-Polinomio operator + (Polinomio X, Polinomio Y){
-  vector<double> v_sum;
-  vector<double> v_X = X.get_pol();
-  vector<double> v_Y = Y.get_pol();
-  for (int i=0; i< v_X.size(); ++i){
-    v_sum.push_back(v_X[i]+v_Y[i]);
-  }; 
-  Vector C(X.get_dim(),v_sum);
-  return C;
-}
-*/
-
-/*//Operador multiplicacion por escalar
-Vector operator * (double x, Vector Y){
-  double escalar = x; 
-  vector<double> v_mul;
-  vector<double> v_Y = Y.get_mt();
-  for (int i=0; i< v_Y.size(); ++i){
-    v_mul.push_back(escalar*v_Y[i]);
-  }; 
-  Vector C(Y.get_dim(),v_mul);
+//Operador Suma
+template <class T>
+Polinomio<T> operator + (Polinomio<T> X, Polinomio<T> Y){
+  vector<T> v_sum;
+  vector<T> v_X = X.get_pol();
+  vector<T> v_Y = Y.get_pol();
+  if (v_X.size()>v_Y.size()){
+    for (int i=0; i<v_X.size(); ++i){
+      v_sum.push_back(v_X[i]+v_Y[i]);
+    }
+  }
+  else{
+    for (int i=0; i< v_Y.size(); ++i){
+      v_sum.push_back(v_X[i]+v_Y[i]);
+    }
+  } 
+  Polinomio<T> C(v_sum);
   return C;
 }
 
-
-//Producto punto
-double operator * (Vector X, Vector Y){
-  vector<double> v_mul;
-  vector<double> v_X = X.get_mt();
-  vector<double> v_Y = Y.get_mt();
-  double C=0.0;
-  for (int i=0; i< v_Y.size(); ++i){
-    C=C+(v_X[i]*v_Y[i]);
-  }; 
+//Operador Resta
+template <class T>
+Polinomio<T> operator - (Polinomio<T> X, Polinomio<T> Y){
+  vector<T> v_sum;
+  vector<T> v_X = X.get_pol();
+  vector<T> v_Y = Y.get_pol();
+  if (v_X.size()>v_Y.size()){
+    for (int i=0; i< v_X.size(); ++i){
+      v_sum.push_back(v_X[i]-v_Y[i]);
+    }
+  }
+  else{
+    for (int i=0; i< v_Y.size(); ++i){
+      v_sum.push_back(v_X[i]-v_Y[i]);
+    }
+  } 
+  Polinomio<T> C(v_sum);
   return C;
 }
 
-*/
+//Operador multiplicacion
+template <class T>
+Polinomio<T> operator * (Polinomio<T> X, Polinomio<T> Y){
+  vector<T> v_producto;
+  int grado= X.get_grado()+Y.get_grado();
+  //llenar el nuevo vector polinomio
+  for (int k=0; k<grado+1; ++k){
+    v_producto.push_back(0.0);
+  }
+  for (int j=Y.get_grado(); j>=0; --j ){
+    for(int i=0; i<=X.get_grado(); ++i){
+      v_producto[i+j]=  v_producto[i+j]+(Y.Coef(j)*X.Coef(i));
+    }
+  }
+  Polinomio<T> C(v_producto);
+  return C;
+}
+
+
+//Operador multiplicacion por escalar
+template <class T>
+Polinomio<T> operator * (T x, Polinomio<T> Y){
+  vector<T> v_producto;
+  vector<T> v_pol = Y.get_pol();
+  int grado= Y.get_grado();
+  //llenar el nuevo vector polinomio
+  for (int k=0; k<grado+1; ++k){
+    v_producto.push_back(x*v_pol[k]);
+  }
+  Polinomio<T> C(v_producto);
+  return C;
+}
+//Funcion que almacena los resultados de la division sintetica tomando como divisor un binomio monico. Se almacena los resultado en un vector donde el v[0]= resto, los demas términos son los coeficientes del polinomio
+template <class T>
+vector<T> Polinomio<T>::div(Polinomio<T> Y){
+  vector<T> v_dividido;
+  T numero = -1*Y.Coef(0);
+  //cout <<pol.size()<<endl; 
+  for(int k=0; k<pol.size(); ++k){
+    v_dividido.push_back(0.0);
+  }
+  for(int i=v_dividido.size(); i>0; --i){
+    v_dividido[i-1]=(v_dividido[i]*numero)+pol[i-1]; 
+  }
+  return v_dividido;
+}
+//operador division sintetica tomando como divisor monomios mónicos usando funcion "div"
+template <class T>
+Polinomio<T> operator / (Polinomio<T> X, Polinomio<T> Y){
+  vector<T> v_nuevo; 
+  vector<T> v_div = X.div(Y);
+  for(int k=1; k<X.get_grado()+1; ++k){
+    v_nuevo.push_back(v_div[k]);
+  }
+  Polinomio<T> C(v_nuevo);
+  return C;
+}
+
+//Operador resto division sintetica usando funcion div 
+template <class T>
+T operator % (Polinomio<T> X, Polinomio<T> Y){
+  return X.div(Y)[0];
+}
 
 
